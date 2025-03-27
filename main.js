c6d4ff90c01a91c035c1b568cc404e2493b6ee98
You Qi <choongyouqi@hotmail.com>
2025-03-27 14:17:41 +0800

    Use environment var port

--- main.js
+++ main.js
@@ -57,4 +57,5 @@ app.post("/decode", upload.single("qrImage"), async (req, res) => {
     }
 })
 
-app.listen(3000, () => console.log("Server running at http://localhost:3000"))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running on port ${PORT} (http://localhost:${PORT})`))
